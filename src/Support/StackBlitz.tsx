cca33f4472aa7036fc137f217fc905b7b136b8cc
konghayao <3446798488@qq.com>
2022-08-06 09:57:17 +0800

    feat: 更改示范案例

--- src/Support/StackBlitz.tsx
+++ src/Support/StackBlitz.tsx
@@ -1,13 +1,13 @@
 import { Component } from "solid-js";
 
-export const Sandbox: Component<{ id: string }> = (props) => {
+export const Sandbox: Component<{ src: string }> = (props) => {
     return (
         <div
             class="w-full"
             style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;"
         >
             <iframe
-                src={`https://codesandbox.io/embed/${props.id}?autoresize=1&expanddevtools=1&fontsize=14&theme=light`}
+                src={props.src}
                 style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;"
                 allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
                 sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
